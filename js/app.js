1d7034da5567edc7eae6f74702f0745369cea498
VMerk <mikulenko.vladimir@yandex.ru>
2020-01-18 23:08:18 +0300

    The error of "jumping" hands when crossing the zero mark is fixed Replaced the main font with Cutive Minor fix styles

--- js/app.js
+++ js/app.js
@@ -1,26 +1,59 @@
-document.addEventListener('DOMContentLoaded', function(){
-  setHandsDegrees()
-  setInterval(setHandsDegrees, 1000)
-});
+{
+  let previousSecondDegrees = 0;
+  let secondMultiplier = 0;
 
-function setHandsDegrees() {
-   const now = new Date();
-   const hour = now.getHours() % 12 || 12;
-   const hourDegrees = 360 / 12 * hour;
-   const minute = now.getMinutes();
-   const minuteDegrees = 360 / 60 * minute;
-   const second = now.getSeconds();
-   const secondDegrees = 360 / 60 * second;
-   const hourHands = document.querySelectorAll('.main-timer .hour-hand');
-   for (const hand of hourHands) {
-     hand.style.transform = `rotate(${hourDegrees}deg)`;
-   }
-   const minuteHands = document.querySelectorAll('.main-timer .minute-hand');
-   for (const hand of minuteHands) {
-     hand.style.transform = `rotate(${minuteDegrees}deg)`;
-   }
-   const secondHands = document.querySelectorAll('.main-timer .second-hand');
-   for (const hand of secondHands) {
-     hand.style.transform = `rotate(${secondDegrees}deg)`;
-   }
+  let previousMinuteDegrees = 0;
+  let minuteMultiplier = 0;
+
+  let previousHourDegrees = 0;
+  let hourMultiplier = 0;
+
+  document.addEventListener('DOMContentLoaded', function(){
+    setHandsDegrees();
+    setInterval(setHandsDegrees, 1000);
+  });
+
+  function setHandsDegrees() {
+    const now = new Date();
+    const hour = now.getHours() % 12 || 12;
+    const hourDegrees = 360 / 12 * hour;
+    const minute = now.getMinutes();
+    const minuteDegrees = 360 / 60 * minute;
+    const second = now.getSeconds();
+    const secondDegrees = (360 / 60 * second);
+
+    // New turn
+    if (secondDegrees - previousSecondDegrees < 0) {
+      secondMultiplier++;
+    }
+    previousSecondDegrees = secondDegrees;
+
+    if (minuteDegrees - previousMinuteDegrees < 0) {
+      minuteMultiplier++;
+    }
+    previousMinuteDegrees = minuteDegrees;
+
+    if (hourDegrees - previousHourDegrees < 0) {
+      hourMultiplier++;
+    }
+    previousHourDegrees = hourDegrees;
+
+    // Hour hands
+    const hourHands = document.querySelectorAll('.main-timer .hour-hand');
+    for (const hand of hourHands) {
+      hand.style.transform = `rotate(${(hourMultiplier * 360) + hourDegrees}deg)`;
+    }
+
+    // Minute hands
+    const minuteHands = document.querySelectorAll('.main-timer .minute-hand');
+    for (const hand of minuteHands) {
+      hand.style.transform = `rotate(${(minuteMultiplier * 360) + minuteDegrees}deg)`;
+    }
+
+    // Second hands
+    const secondHands = document.querySelectorAll('.main-timer .second-hand');
+    for (const hand of secondHands) {
+      hand.style.transform = `rotate(${(secondMultiplier * 360) + secondDegrees}deg)`;
+    }
+  }
 }

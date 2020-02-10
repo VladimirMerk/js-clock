dcbe29ba554bf0d99450835e3860617aaab270d0
VMerk <mikulenko.vladimir@yandex.ru>
2020-02-10 23:44:02 +0300

    Stopwatch now works

--- js/app.js
+++ js/app.js
@@ -9,8 +9,12 @@
   let hourMultiplier = 0;
 
   document.addEventListener('DOMContentLoaded', function(){
-    setHandsDegrees();
-    setInterval(setHandsDegrees, 1000);
+    setInterval(setHandsDegrees(), 1000);
+  });
+  document.addEventListener('click', function(e){
+    if (e.target.classList.contains('stopwatch')) {
+      runStopwatch(e.target)
+    }
   });
 
   function setHandsDegrees() {
@@ -55,6 +59,36 @@
     const secondHands = document.querySelectorAll('.main-timer .second-hand');
     for (const hand of secondHands) {
       hand.style.transform = `rotate(${(secondMultiplier * 360) + secondDegrees}deg)`;
+    }
+    return setHandsDegrees;
+  }
+
+  function runStopwatch(stopwatchElement) {
+    if (! stopwatchElement.dataset.hasOwnProperty('start')) {
+      const millisecondHand = stopwatchElement.querySelector('.millisecond-hand');
+      const secondHand = stopwatchElement.querySelector('.second-hand');
+      stopwatchElement.dataset.start = Date.now() / 10;
+      let milliSeconds = 0;
+      stopwatchElement.timer = setInterval(() => {
+        milliSeconds = Date.now() / 10
+        const passedMilliseconds = milliSeconds - stopwatchElement.dataset.start;
+        if (millisecondHand !== null) {
+          const milliSecondsDegrees = passedMilliseconds % 100 / 100 * 360
+          millisecondHand.style.transform = `rotate(${milliSecondsDegrees}deg)`;
+        }
+        if (secondHand !== null) {
+          const secondsDegrees =  passedMilliseconds / 100 % 60 / 100 * 360
+          secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+        }
+      }, 50);
+    } else {
+      milliSeconds = Date.now() / 10
+      const passedMilliseconds = milliSeconds - stopwatchElement.dataset.start;
+      let seconds = parseFloat(passedMilliseconds / 100 % 60).toFixed(4);
+      let minutes = Math.floor(passedMilliseconds / 100 / 60 % 60);
+      seconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+      minutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
+      console.log('Pass', `${minutes}:${seconds}`);
     }
   }
 }

22025e6ca496d6aa7f61d091e2c52d9d07321e31
VMerk <mikulenko.vladimir@yandex.ru>
2020-02-20 23:17:57 +0300

    Fix stopwatch hand Added the stopwatch stop at double click Added display of stopwatch results Fixed layout of the stopwatch hands

--- js/app.js
+++ js/app.js
@@ -11,9 +11,29 @@
   document.addEventListener('DOMContentLoaded', function(){
     setInterval(setHandsDegrees(), 1000);
   });
+
+  startTimeout = null;
+
   document.addEventListener('click', function(e){
     if (e.target.classList.contains('stopwatch')) {
-      runStopwatch(e.target)
+      clearTimeout(startTimeout)
+      startTimeout = setTimeout(() => {
+        onStopwatch(e.target)
+      }, 200)
+    }
+    if (
+      e.target.parentElement &&
+      e.target.parentElement.id === 'list' &&
+      !e.target.nextElementSibling
+    ) {
+      resetResult()
+    }
+  });
+
+  document.addEventListener('dblclick', function(e){
+    if (e.target.classList.contains('stopwatch')) {
+      clearTimeout(startTimeout)
+      resetStopwatch(e.target)
     }
   });
 
@@ -63,32 +83,76 @@
     return setHandsDegrees;
   }
 
-  function runStopwatch(stopwatchElement) {
+  function onStopwatch(stopwatchElement) {
     if (! stopwatchElement.dataset.hasOwnProperty('start')) {
-      const millisecondHand = stopwatchElement.querySelector('.millisecond-hand');
-      const secondHand = stopwatchElement.querySelector('.second-hand');
-      stopwatchElement.dataset.start = Date.now() / 10;
-      let milliSeconds = 0;
-      stopwatchElement.timer = setInterval(() => {
-        milliSeconds = Date.now() / 10
-        const passedMilliseconds = milliSeconds - stopwatchElement.dataset.start;
-        if (millisecondHand !== null) {
-          const milliSecondsDegrees = passedMilliseconds % 100 / 100 * 360
-          millisecondHand.style.transform = `rotate(${milliSecondsDegrees}deg)`;
-        }
-        if (secondHand !== null) {
-          const secondsDegrees =  passedMilliseconds / 100 % 60 / 100 * 360
-          secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-        }
-      }, 50);
+      runStopwatch(stopwatchElement)
     } else {
+      const result = getCurrentResult(stopwatchElement)
+      inserResult(result)
+    }
+  }
+
+  function runStopwatch (stopwatchElement) {
+    const millisecondHand = stopwatchElement.querySelector('.millisecond-hand');
+    const secondHand = stopwatchElement.querySelector('.second-hand');
+    stopwatchElement.dataset.start = Date.now() / 10;
+    let milliSeconds = 0;
+    stopwatchElement.timer = setInterval(() => {
       milliSeconds = Date.now() / 10
       const passedMilliseconds = milliSeconds - stopwatchElement.dataset.start;
-      let seconds = parseFloat(passedMilliseconds / 100 % 60).toFixed(4);
-      let minutes = Math.floor(passedMilliseconds / 100 / 60 % 60);
-      seconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
-      minutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
-      console.log('Pass', `${minutes}:${seconds}`);
+      if (millisecondHand !== null) {
+        const milliSecondsDegrees = passedMilliseconds % 100 / 100 * 360
+        millisecondHand.style.transform = `rotate(${milliSecondsDegrees}deg)`;
+      }
+      if (secondHand !== null) {
+        const secondsDegrees =  passedMilliseconds / 100 % 60 / 100 * 360 * (100 / 60)
+        secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+      }
+    }, 50);
+  }
+
+  function resetStopwatch (stopwatchElement) {
+    if (stopwatchElement.dataset.hasOwnProperty('start')) {
+      clearInterval(stopwatchElement.timer)
+      const millisecondHand = stopwatchElement.querySelector('.millisecond-hand');
+      const secondHand = stopwatchElement.querySelector('.second-hand');
+      millisecondHand.style.removeProperty('transform');
+      secondHand.style.removeProperty('transform');
+      delete stopwatchElement.dataset.start;
+      delete stopwatchElement.dataset.previous;
+      delete stopwatchElement.dataset.previous;
     }
+  }
+
+  function getCurrentResult (stopwatchElement) {
+    milliSeconds = Date.now() / 10;
+    const passedMilliseconds = milliSeconds - stopwatchElement.dataset.start;
+    let result = milliSecondsToHuman(passedMilliseconds);
+    if (stopwatchElement.dataset.previous) {
+      const passedMillisecondsPrevious = milliSeconds - stopwatchElement.dataset.previous;
+      result += ` | ${milliSecondsToHuman(passedMillisecondsPrevious)}`;
+    }
+    stopwatchElement.dataset.previous = milliSeconds;
+    return result;
+  }
+
+  function milliSecondsToHuman(milliseconds) {
+    let seconds = parseFloat(milliseconds / 100 % 60).toFixed(3);
+    let minutes = Math.floor(milliseconds / 100 / 60 % 60);
+    seconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+    minutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
+    return `${minutes}:${seconds}`
+  }
+
+  function inserResult(str) {
+    const ul = document.getElementById('list');
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(`${ul.childElementCount + 1} ${str}`));
+    ul.appendChild(li);
+  }
+
+  function resetResult() {
+    const ul = document.getElementById('list');
+    ul.innerHTML = "";
   }
 }
